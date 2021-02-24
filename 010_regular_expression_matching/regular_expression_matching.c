48c73090bcef65e5ed0548684d4ed03c8a8799d2
marshall <gongmarshall@github.com>
2021-02-24 22:58:43 +0800

    010 modifing

--- 010_regular_expression_matching/regular_expression_matching.c
+++ 010_regular_expression_matching/regular_expression_matching.c
@@ -4,6 +4,76 @@
 
 bool isMatch(char *s, char *p)
 {
+	char* q=p;
+//	printf("s:%s, p:%s\n", s, p);
+	while(*s != '\0' && *p != '\0')
+	{
+//		printf("s:%c, p:%c\n", *s, *p);
+		if(*p == '.')
+		{
+			s++;
+			p++;
+//			printf("1 s:%c, p:%c\n", *s, *p);
+		}
+		else if(*p == '*')
+		{
+//			printf("1\n");
+			if(p == q)
+			{
+//				printf("p++\n");
+				p++;
+			}
+			else 
+			{
+				if(*(p-1) == '.')
+				{
+//					printf("2\n");
+					while(*s != '\0')
+						s++;
+				}
+				else
+				{
+//					printf("p-1:%c\n", *(p-1));
+					while(*s == *(p-1))
+						s++;
+					p++;
+//					printf("p++ 2\n");
+				}
+			}
+		}
+		else
+		{
+//			printf("3\n");
+		       	if(*s != *p)
+				if(*(p+1) == '*')
+				{
+					s++;
+					p++;
+					p++;
+				}
+				else
+					return false;
+			else
+			{
+				s++;
+				p++;
+			}
+//			printf("3 s:%c, p:%c\n", *s, *p);	
+		}
+//		printf("4\n");
+	}
 	
+//	printf("s: %c, p: %c\n", *s, *p);
+	while(*p == '*')
+		p++;
+	if(*s != '\0' || *p != '\0')
+		return false;
+	else
+		return true;
 }
 
+int main(int argc, char *argv[])
+{
+	printf("%s\n", isMatch(argv[1], argv[2])?"true":"false");
+	return 0;
+}
